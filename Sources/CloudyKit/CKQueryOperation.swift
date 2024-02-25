e954c18256fb695dcf20ed67b47abe90db8d9745
Ragnar Henriksen <ragnarhenriksen@mac.com>
2024-02-25 11:58:26 +0100

    Adds CKQueryOperation

---
+++ Sources/CloudyKit/CKQueryOperation.swift
@@ -0,0 +1,33 @@
+//
+//  CKQueryOperation.swift
+//
+//
+//  Created by Ragnar Henriksen on 25/02/2024.
+//
+
+import Foundation
+
+public class CKQueryOperation {
+    public class Cursor {
+        internal let continuationMarker: String
+
+        public init(continuationMarker: String) {
+            self.continuationMarker = continuationMarker
+        }
+    }
+
+    public static let maximumResults: Int = 200
+
+    public var query: CKQuery?
+    public var cursor: CKQueryOperation.Cursor?
+
+    public convenience init(query: CKQuery) {
+        self.init()
+        self.query = query
+    }
+
+    public convenience init(cursor: CKQueryOperation.Cursor) {
+        self.init()
+        self.cursor = cursor
+    }
+}
